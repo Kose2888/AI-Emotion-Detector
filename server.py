0cc542f5da14fb8848d526ef5b175ee2af560b64
Kose2888 <ethan.kostiuk@gmail.com>
2026-02-01 20:32:38 -0500

    Fixed static analysis

--- server.py
+++ server.py
@@ -1,3 +1,6 @@
+"""
+This module deploys the web server.
+"""
 from flask import Flask, render_template, request
 from EmotionDetection.emotion_detection import emotion_detector
 
@@ -5,10 +8,12 @@ app = Flask("Emotion Detector")
 
 @app.route("/")
 def render_index_page():
+    """Renders the index page for the root GET request"""
     return render_template("index.html")
 
 @app.route("/emotionDetector")
 def emotion_analyser():
+    """returns all emotion key-value pairs and the dominant emotion as a string"""
     text_to_analyse = request.args.get('textToAnalyze')
 
     emotion = emotion_detector(text_to_analyse)
@@ -16,20 +21,20 @@ def emotion_analyser():
     # Handle blank entries
     if emotion is None:
         return "Invalid text! Please try again!"
-    else:
-        first_four_emotions = dict(list(emotion.items())[:4])
-        # Cast to string and remove the outside brackets 
-        first_four_emotions_string = str(first_four_emotions)[1:-1]
 
-        fifth_emotion = dict(list(emotion.items())[4:5])
-        fifth_emotion_string = str(fifth_emotion)[1:-1]
+    first_four_emotions = dict(list(emotion.items())[:4])
+    # Cast to string and remove the outside brackets
+    first_four_emotions_string = str(first_four_emotions)[1:-1]
 
-        dominant_emotion = emotion['dominant_emotion']
+    fifth_emotion = dict(list(emotion.items())[4:5])
+    fifth_emotion_string = str(fifth_emotion)[1:-1]
 
-        formatted_response = f"""{first_four_emotions_string} and {fifth_emotion_string}.
-        The dominant emotion is <strong>{dominant_emotion}</strong>."""
+    dominant_emotion = emotion['dominant_emotion']
 
-        return formatted_response
+    formatted_response = f"""{first_four_emotions_string} and {fifth_emotion_string}.
+    The dominant emotion is <strong>{dominant_emotion}</strong>."""
+
+    return formatted_response
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5001)
